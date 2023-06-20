dda0e2cdbca785313d5f30614b5f94838ff060bf
aboubakdiallo <babacardiallo2121@gmail.com>
2023-06-20 12:23:54 +0000

    Docker

--- script.sh
+++ script.sh
@@ -1,18 +1,23 @@
 # Construit une image Docker en utilisant le Dockerfile spécifié (-f) et lui attribue le tag (-t) "image_test"
-sudo docker image build -f Dockerfile -t image_test .
-
+docker image build -f Dockerfile -t image_test .
+echo "                              --                                    "
+echo "                              --                                    "
 # Exécute un conteneur Docker à partir de l'image "image_test", en mappant le port 8080 de l'hôte au port 8080 du conteneur,
 # en mode détaché (--detach) et en donnant un nom (--name) "testing" au conteneur
-sudo docker container run -p 8080:8080 --detach --name testing image_test
-
+docker container run -p 8080:8080 --detach --name testing image_test
+echo "                              --                                    "
+echo "                              --                                    "
 # Exécute une commande interactive dans le conteneur "testing" en démarrant un shell bash (/bin/bash)
-sudo docker exec -it testing /bin/bash
-
+docker exec -it testing /bin/bash
+echo "                              --                                    "
+echo "                              --                                    "
 # Liste les images Docker présentes sur le système
-sudo docker image ls
-
+docker image ls
+echo "                              --                                    "
+echo "                              --                                    "
 # Liste les conteneurs Docker en cours d'exécution
-sudo docker ps
-
+docker ps
+echo "                              --                                    "
+echo "                              --                                    "
 # Exécute un conteneur Docker à partir de l'image "image_test" (par défaut en mode interactif)
-sudo docker container run image_test
+docker container run image_test
